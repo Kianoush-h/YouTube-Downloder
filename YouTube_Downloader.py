0a1c50fb3962d28ed5b33a15fd686fee68ed1958
Kianoush Haratiannejadi <haratiank2@gmail.com>
2020-09-03 10:57:09 -0400

    Update YouTube_Downloader.py

--- YouTube_Downloader.py
+++ YouTube_Downloader.py
@@ -6,7 +6,11 @@ GitHUb:https://github.com/Kianoush-h
 YouTube: https://www.youtube.com/channel/UCvf9_53f6n3YjNEA4NxAkJA?view_as=subscriber
 LinkedIn: https://www.linkedin.com/in/kianoush-haratiannejadi/
 
+Email: haratiank2@gmail.com
+
 """
+
+
 
 
 import tkinter as tk 
